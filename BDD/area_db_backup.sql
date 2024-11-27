76aacc5cf420d5e572942abef5d257c16eff9de2
Alex Aubry <alex.aubry@epitech.eu>
2024-11-27 15:19:31 +0100

    [ADD] create_actions_table.sql

--- BDD/area_db_backup.sql
+++ BDD/area_db_backup.sql
@@ -21,6 +21,44 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: actions; Type: TABLE; Schema: public; Owner: area_user
+--
+
+CREATE TABLE public.actions (
+    id integer NOT NULL,
+    service_id integer NOT NULL,
+    name character varying(100) NOT NULL,
+    description text,
+    parameters jsonb,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+
+ALTER TABLE public.actions OWNER TO area_user;
+
+--
+-- Name: actions_id_seq; Type: SEQUENCE; Schema: public; Owner: area_user
+--
+
+CREATE SEQUENCE public.actions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.actions_id_seq OWNER TO area_user;
+
+--
+-- Name: actions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: area_user
+--
+
+ALTER SEQUENCE public.actions_id_seq OWNED BY public.actions.id;
+
+
+--
 -- Name: services; Type: TABLE; Schema: public; Owner: area_user
 --
 
@@ -96,6 +134,13 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: actions id; Type: DEFAULT; Schema: public; Owner: area_user
+--
+
+ALTER TABLE ONLY public.actions ALTER COLUMN id SET DEFAULT nextval('public.actions_id_seq'::regclass);
+
+
+--
 -- Name: services id; Type: DEFAULT; Schema: public; Owner: area_user
 --
 
@@ -107,6 +152,14 @@ ALTER TABLE ONLY public.services ALTER COLUMN id SET DEFAULT nextval('public.ser
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: actions; Type: TABLE DATA; Schema: public; Owner: area_user
+--
+
+COPY public.actions (id, service_id, name, description, parameters, created_at) FROM stdin;
+\.
 
 
 --
@@ -126,6 +179,13 @@ COPY public.users (id, username, email, password_hash, created_at, updated_at) F
 
 
 --
+-- Name: actions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: area_user
+--
+
+SELECT pg_catalog.setval('public.actions_id_seq', 1, false);
+
+
+--
 -- Name: services_id_seq; Type: SEQUENCE SET; Schema: public; Owner: area_user
 --
 
@@ -137,6 +197,14 @@ SELECT pg_catalog.setval('public.services_id_seq', 1, false);
 --
 
 SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+
+
+--
+-- Name: actions actions_pkey; Type: CONSTRAINT; Schema: public; Owner: area_user
+--
+
+ALTER TABLE ONLY public.actions
+    ADD CONSTRAINT actions_pkey PRIMARY KEY (id);
 
 
 --
@@ -161,6 +229,14 @@ ALTER TABLE ONLY public.users
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: actions fk_service_id; Type: FK CONSTRAINT; Schema: public; Owner: area_user
+--
+
+ALTER TABLE ONLY public.actions
+    ADD CONSTRAINT fk_service_id FOREIGN KEY (service_id) REFERENCES public.services(id) ON DELETE CASCADE;
 
 
 --
