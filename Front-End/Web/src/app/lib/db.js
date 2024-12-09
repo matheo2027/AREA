590ff77836528debc57280946a2d528f0df4cee0
matheo2027 <matheo.piques@epitech.eu>
2024-12-09 08:28:08 +0100

    [DLT] Useless file

--- Front-End/Web/src/app/lib/db.js
+++
@@ -1,9 +0,0 @@
-import { Pool } from 'pg';
-
-export const pool = new Pool({
-    user: 'postgres',       // Remplace par ton utilisateur PostgreSQL
-    host: 'localhost',      // Adresse du serveur
-    database: 'user_management', // Nom de la base
-    password: '123',   // Ton mot de passe
-    port: 5432,             // Port par défaut
-});
