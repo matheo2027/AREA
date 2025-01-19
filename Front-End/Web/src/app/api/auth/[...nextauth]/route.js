0a0b682aa879fd9229cd6b6e22e4b3bba2e2bfc4
babacar sow <babacar.sow@epitech.eu>
2025-01-19 18:44:33 +0100

    [ADD] adding oauth for mobile

--- Front-End/Web/src/app/api/auth/[...nextauth]/route.js
+++ Front-End/Web/src/app/api/auth/[...nextauth]/route.js
@@ -20,14 +20,32 @@ const handler = NextAuth({
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, account, profile }) {
       if (account) {
         token.accessToken = account.access_token;
+        token.provider = account.provider;
+        token.email = profile.email;
       }
       return token;
     },
     async session({ session, token }) {
       session.accessToken = token.accessToken;
+      session.provider = token.provider;
+      session.email = token.email;
+
+      // Sauvegarder l'email, le token et le type de connexion
+      await fetch("http://localhost:8080/api/save-user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: token.email,
+          token: token.accessToken,
+          connexion: token.provider,
+        }),
+      });
+
       return session;
     },
   },
